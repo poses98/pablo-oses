ada2c289c6d15a40dfaeea3e6b9ba7f7d3df4d10
poses98 <posesand@gmail.com>
2024-04-18 01:06:11 +0200

    calculating height at running time

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,12 +1,21 @@
 import TaskBar from '@/components/TaskBar/TaskBar';
 import styles from './basiclayout.module.css';
 import { Inter } from 'next/font/google';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function BasicLayout({ children }) {
+  const [innerHeight, setInnerHeight] = useState('100vh');
+  useEffect(() => {
+    setInnerHeight(`${window.innerHeight}px`);
+    console.log(innerHeight);
+  }, []);
   return (
-    <div className={`${styles.container} ${inter.className}`}>
+    <div
+      className={`${styles.container} ${inter.className}`}
+      style={{ height: innerHeight }}
+    >
       <div className={styles.backgroundLettering}>
         <h1>Pablo Osés Andía</h1>
         <h2>Full-Stack developer</h2>
