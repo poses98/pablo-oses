3cf567fe023d6675095566e976f7acdd6b5a4e14
poses98 <posesand@gmail.com>
2024-04-18 00:43:14 +0200

    added background

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -7,7 +7,12 @@ const inter = Inter({ subsets: ['latin'] });
 export default function BasicLayout({ children }) {
   return (
     <div className={`${styles.container} ${inter.className}`}>
+      <div className={styles.backgroundLettering}>
+        <h1>Pablo Osés Andía</h1>
+        <h2>Full-Stack developer</h2>
+      </div>
       {children}
+
       <TaskBar />
     </div>
   );
