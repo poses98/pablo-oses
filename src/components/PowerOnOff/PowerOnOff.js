537c36f63da427c7d2bb0716d2a783a1f370df14
poses98 <posesand@gmail.com>
2024-04-18 11:26:15 +0200

    started poweronoff

---
+++ src/components/PowerOnOff/PowerOnOff.js
@@ -0,0 +1,9 @@
+import styles from './poweronoff.module.css';
+
+export default function PowerOnOff({ handleClick }) {
+  return (
+    <div className={styles.container}>
+      <h2>Click the screen to start</h2>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,11 @@ import OSDesktop from '@/components/OSDesktop/OSDesktop';
 import BasicLayout from '@/layouts/BasicLayout';
 import OSWindow from '@/components/OSWindow/OSWindow';
 import { tree } from '@/resources/tree';
+import PowerOnOff from '@/components/PowerOnOff/PowerOnOff';
 
 export default function Home() {
   const [windows, setWindows] = useState([]);
+  const [started, setStarted] = useState(false);
 
   const spawnWindow = (node) => {
     const windowContent = {
@@ -26,18 +28,20 @@ export default function Home() {
   };
 
   return (
-    <BasicLayout>
-      {windows.map((window, index) => (
-        <OSWindow
-          key={index}
-          name={window.name}
-          type={window.type}
-          route={window.route ? window.route : null}
-          content={window.content}
-          handleClose={() => handleWindowClose(window.id)}
-        />
-      ))}
-      <OSDesktop icons={tree} onIconClick={spawnWindow} />
-    </BasicLayout>
+    <>
+      <BasicLayout>
+        {windows.map((window, index) => (
+          <OSWindow
+            key={index}
+            name={window.name}
+            type={window.type}
+            route={window.route ? window.route : null}
+            content={window.content}
+            handleClose={() => handleWindowClose(window.id)}
+          />
+        ))}
+        <OSDesktop icons={tree} onIconClick={spawnWindow} />
+      </BasicLayout>
+    </>
   );
 }
