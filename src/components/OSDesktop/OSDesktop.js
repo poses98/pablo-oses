06110ee86564389766db6623b3958903a5182689
poses98 <posesand@gmail.com>
2024-04-18 18:23:51 +0200

    cleaned unused imports

--- src/components/OSDesktop/OSDesktop.js
+++ src/components/OSDesktop/OSDesktop.js
@@ -1,16 +1,10 @@
-import { useState, useEffect } from 'react';
 import OSIcon from '../OSIcon/OSIcon';
 import styles from './osdesktop.module.css';
 
 export default function OSDesktop({ icons, onIconClick }) {
-  const [tree, setTree] = useState([]);
-  useEffect(() => {
-    setTree(icons);
-  }, [icons]);
-
   return (
     <div className={styles.container}>
-      {tree.map((element, index) => {
+      {icons.map((element, index) => {
         return (
           <OSIcon
             key={index}
