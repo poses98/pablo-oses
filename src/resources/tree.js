8d1183962bf7256cd919d29759174ccc45ed1986
poses98 <posesand@gmail.com>
2024-04-22 22:27:08 +0200

    updated folder order

--- src/resources/tree.js
+++ src/resources/tree.js
@@ -68,12 +68,6 @@ export const tree = [
     ],
   },
   {
-    name: 'Contact Me',
-    type: 'contact',
-    content: '**Lorem** ipsum dolor sit amet.',
-    route: 'contact',
-  },
-  {
     name: 'Curriculum Vitae',
     type: 'pdf',
     content: '**Lorem** ipsum dolor sit amet.',
@@ -85,5 +79,11 @@ export const tree = [
     content:
       '### Who Am I?\n\nHello! My name is **Pablo**, and I am a **Full Stack Developer** with a keen interest in software engineering. I’m ready to gain practical experience and expand my knowledge. While I’m relatively new to the professional scene, I’ve been developing my skills not only through personal projects but also through client projects.\n\n### My Focus\n\nI focus on **SOLID principles** and design patterns, aiming to write clean, efficient, and scalable code. I enjoy using technologies like **Next.js** to build applications that are centered around the user.\n\n### Growth Mindset\n\nI’m a quick learner with a growth mindset, always ready to pick up new skills and stay updated with evolving industry trends.\n\n### Looking for Opportunities\n\nI’m actively looking for opportunities where I can contribute and learn from experienced professionals. Let’s connect and discuss how I can add value to your projects while gaining valuable real-world experience. Looking forward to our conversation!',
     route: 'who-am-i',
+  },
+  {
+    name: 'Contact Me',
+    type: 'contact',
+    content: '**Lorem** ipsum dolor sit amet.',
+    route: 'contact',
   },
 ];
