5cfcf85713b297e76c3e48a313fc76cef731869a
poses98 <posesand@gmail.com>
2024-04-22 13:01:00 +0200

    Changed mobile width

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,4 +1,5 @@
 import TaskBar from '@/components/TaskBar/TaskBar';
+import Head from 'next/head';
 import styles from './basiclayout.module.css';
 import { Inter } from 'next/font/google';
 import { useEffect, useState } from 'react';
@@ -18,17 +19,108 @@ export default function BasicLayout({ children }) {
   }, []);
 
   return (
-    <main
-      className={`${styles.container} ${inter.className}`}
-      style={{ height: innerHeight }}
-    >
-      <div className={styles.backgroundLettering}>
-        <h1>Pablo Osés Andía</h1>
-        <h2>Full-Stack developer</h2>
-      </div>
-      {children}
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Transformamos su hogar en un ecosistema inteligente que se adapta a su estilo de vida y mejora su comodidad y seguridad."
+        />
+        <meta name="author" content={name} />
+        <meta property="og:image" content={'/img/og_image.jpg'} />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `history.scrollRestoration = "manual"`,
+          }}
+        />
+        <title>Pablo Osés</title>
+      </Head>
+      <main
+        className={`${styles.container} ${inter.className}`}
+        style={{ height: innerHeight }}
+      >
+        <div className={styles.backgroundLettering}>
+          <h1>Pablo Osés Andía</h1>
+          <h2>Full-Stack developer</h2>
+        </div>
+        {children}
 
-      <TaskBar />
-    </main>
+        <TaskBar />
+      </main>
+    </>
   );
 }
