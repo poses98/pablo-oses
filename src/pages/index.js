ff58400f50cb89cd7330a8400cdad187eaa58c6d
poses98 <posesand@gmail.com>
2024-04-24 16:37:02 +0200

    cleaned up component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,15 @@ import { useState } from 'react';
 import OSDesktop from '@/components/OSDesktop/OSDesktop';
 import BasicLayout from '@/layouts/BasicLayout';
 import OSWindow from '@/components/OSWindow/OSWindow';
-import { tree } from '@/resources/tree';
 import { useWindowsContext } from '@/providers/WindowsProvider';
 
 export default function Home() {
-  const { windows, spawnWindow, handleWindowClose } = useWindowsContext();
+  const { windows, handleWindowClose } = useWindowsContext();
 
   return (
     <>
       <BasicLayout>
-        {windows.map((window, index) => (
+        {windows.map((window) => (
           <OSWindow
             key={window.id}
             name={window.name}
@@ -22,7 +21,7 @@ export default function Home() {
             handleClose={() => handleWindowClose(window.id)}
           />
         ))}
-        <OSDesktop icons={tree} onIconClick={spawnWindow} />
+        <OSDesktop />
       </BasicLayout>
     </>
   );
