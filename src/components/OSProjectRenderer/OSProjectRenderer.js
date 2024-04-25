cf29562a871265f2d4a8843107c7682a2c526fb1
poses98 <posesand@gmail.com>
2024-04-25 13:29:13 +0200

    added scrollbar

--- src/components/OSProjectRenderer/OSProjectRenderer.js
+++ src/components/OSProjectRenderer/OSProjectRenderer.js
@@ -3,7 +3,12 @@ import OSMarkdown from '../OSMarkdown/OSMarkdown';
 export default function OSProjectRenderer({ route }) {
   return (
     <div
-      style={{ backgroundColor: '#fafafa', height: '100%', padding: '30px' }}
+      style={{
+        backgroundColor: '#fafafa',
+        height: '100%',
+        padding: '30px',
+        overflow: 'scroll',
+      }}
     >
       <OSMarkdown route={route} />
     </div>
