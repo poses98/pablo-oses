7d2a659fe2bce62ca1811b38b609078e4b69d942
poses98 <posesand@gmail.com>
2024-04-25 14:52:52 +0200

    cleaned up component

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,20 @@ export default function Home() {
   const { windows, handleWindowClose } = useWindowsContext();
 
   return (
-    <>
-      <BasicLayout>
-        {windows.map((window) => (
-          <OSWindow
-            key={window.id}
-            name={window.name}
-            type={window.type}
-            route={window.route ? window.route : null}
-            content={window.content}
-            id={window.id}
-            customWindow={window}
-            handleClose={() => handleWindowClose(window.id)}
-          />
-        ))}
-        <OSDesktop />
-      </BasicLayout>
-    </>
+    <BasicLayout>
+      {windows.map((window) => (
+        <OSWindow
+          key={window.id}
+          name={window.name}
+          type={window.type}
+          route={window.route ? window.route : null}
+          content={window.content}
+          id={window.id}
+          customWindow={window}
+          handleClose={() => handleWindowClose(window.id)}
+        />
+      ))}
+      <OSDesktop />
+    </BasicLayout>
   );
 }
