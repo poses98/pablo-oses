badb78ab0fe6380a9ff70fa5819ce6263c57eb89
poses98 <posesand@gmail.com>
2024-04-26 13:36:44 +0200

    target blank

--- src/components/OSProjectRenderer/ActionButton/ActionButton.js
+++ src/components/OSProjectRenderer/ActionButton/ActionButton.js
@@ -3,7 +3,7 @@ import styles from './actionbutton.module.css';
 export default function ActionButton({ text, url }) {
   return (
     <div className={styles.actionButton}>
-      <a href={url} className={styles.button}>
+      <a href={url} target="_blank" className={styles.button}>
         Visit Site
       </a>
     </div>
