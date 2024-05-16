423193327e5822dec443a799742fc0ca5302059b
poses98 <posesand@gmail.com>
2024-05-16 12:07:23 +0200

    feat: Improve layout and styling of Hello World page
    This commit modifies the `page.tsx` file to improve the layout and styling of the Hello World page. The changes include adding a flexbox container with centered alignment and vertical and horizontal spacing. This enhances the visual presentation of the page and provides a better user experience.

--- src/app/hello-world/page.tsx
+++ src/app/hello-world/page.tsx
@@ -1,6 +1,13 @@
 export default async function HelloWorld() {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        minHeight: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <h1>Hello world!😉</h1>
     </div>
   );
