d59766be3df86d5ab0864e5bcbb95b4743e6ccf8
poses98 <posesand@gmail.com>
2024-07-06 17:40:20 +0200

    feat: Add Google Analytics tracking to _app.js
    This commit adds Google Analytics tracking to the _app.js file by including the necessary script and configuration. With Google Analytics, we can gather valuable insights about user interactions and behavior on our application.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
 import '@/styles/globals.css';
 import { WindowsProvider } from '@/providers/WindowsProvider';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <WindowsProvider>
-      <GoogleTagManager gtmId="G-40TDHEKVV5" />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-40TDHEKVV5"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-40TDHEKVV5');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </WindowsProvider>
   );
