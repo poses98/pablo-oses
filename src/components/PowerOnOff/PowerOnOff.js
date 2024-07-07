f4be719315b99f8935d1c1e29838bbd50c646681
poses98 <posesand@gmail.com>
2024-07-07 16:25:41 +0200

    feat: Add lazy loading for PowerOnOff component
    This commit adds lazy loading functionality to the PowerOnOff component. By using the useState hook, the component now tracks whether the screen has been touched and whether it has finished loading. The component also includes a loading animation and a click event handler to trigger the handleClick function. These changes enhance the user experience by providing visual feedback and improving the performance of the component.

--- src/components/PowerOnOff/PowerOnOff.js
+++ src/components/PowerOnOff/PowerOnOff.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import styles from './poweronoff.module.css';
 
 export default function PowerOnOff({ handleClick }) {
+  const [screenTouched, setScreenTouched] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   return (
-    <div className={styles.container}>
-      <h2>Click the screen to start</h2>
+    <div
+      className={`${styles.container} ${loaded ? styles.powerOnAnimation : ''}`}
+      onClick={() => {
+        setScreenTouched(true);
+        setTimeout(() => {
+          setLoaded(true);
+          setTimeout(() => {
+            handleClick();
+          }, 2000);
+        }, 2000);
+      }}
+    >
+      {!screenTouched && (
+        <>
+          <h1 className={styles.vintageText}>Greetings, esteemed visitor!</h1>
+          <h2 className={`${styles.flickerText} ${styles.vintageText}`}>
+            Click the screen to commence
+          </h2>
+        </>
+      )}
+
+      {screenTouched && (
+        <div className={styles.loading}>
+          <h2 className={`${styles.loadingText} ${styles.vintageText}`}>
+            {loaded ? 'Loading accomplished.' : 'Loading...'}
+          </h2>
+          <div className={styles.loadingBox}>
+            <div className={styles.loadingBar} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,35 @@
+import React, { useState } from 'react';
 import OSDesktop from '@/components/OSDesktop/OSDesktop';
 import BasicLayout from '@/layouts/BasicLayout';
 import OSWindow from '@/components/OSWindow/OSWindow';
 import { useWindowsContext } from '@/providers/WindowsProvider';
+import PowerOnOff from '@/components/PowerOnOff/PowerOnOff';
 
 export default function Home() {
   const { windows, handleWindowClose } = useWindowsContext();
+  const [animationPlayed, setAnimationPlayed] = useState(false);
 
   return (
-    <BasicLayout>
-      {windows.map((window) => (
-        <OSWindow
-          key={window.id}
-          name={window.name}
-          type={window.type}
-          route={window.route ? window.route : null}
-          content={window.content}
-          id={window.id}
-          customWindow={window}
-          handleClose={() => handleWindowClose(window.id)}
-        />
-      ))}
-      <OSDesktop />
-    </BasicLayout>
+    <>
+      {animationPlayed && (
+        <BasicLayout>
+          {windows.map((window) => (
+            <OSWindow
+              key={window.id}
+              name={window.name}
+              type={window.type}
+              route={window.route ? window.route : null}
+              content={window.content}
+              id={window.id}
+              customWindow={window}
+              handleClose={() => handleWindowClose(window.id)}
+            />
+          ))}
+          <OSDesktop />
+        </BasicLayout>
+      )}
+
+      <PowerOnOff handleClick={() => setAnimationPlayed(true)} />
+    </>
   );
 }
